d4a49c4b171c5b68577c03a15ea336d6ca563b53
sahartorkaman <st1986.spring@gmail.com>
2024-10-07 22:40:59 +0330

    second  commit

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -24,19 +23,7 @@ func main() {
 	}
 
 	fmt.Println(response)
-	//2
-	//output, err := createErrorMethod2(0)
-	//if err != nil {
-	//	fmt.Println(err)
-	//	return
-	//}
-	//fmt.Println(output)
-	//1
-	//x := Sum(2, 7)
-	//fmt.Printf("%d\n", x)
-	//
-	//y := Sum(2.5, 7.5)
-	//fmt.Printf("%f\n", y)
+
 }
 func (error HttpError) Error() string {
 	return fmt.Sprintf("Http error occurred: %d %s", error.StatusCode, error.Message)
@@ -61,20 +48,4 @@ func GetRequest(url string) (string, error) {
 		return "", NewHttpError(200, "Body is empty")
 	}
 	return string(responseBody), nil
-}
-func Sum[T Number](a, b T) T {
-	return a + b
-}
-func createErrorMethod1(number int) (int, error) {
-	if number == 0 {
-		return 0, errors.New("Number is not valid")
-	}
-	return number * 5, nil
-}
-
-func createErrorMethod2(number int) (int, error) {
-	if number == 0 {
-		return 0, fmt.Errorf("Number is not valid: %d", number)
-	}
-	return number * 5, nil
 }
